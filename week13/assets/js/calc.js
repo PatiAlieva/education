32e90c5b6f195c9c5f5f8d87b6307db1e9b662f1
PatiAlieva <98231797+PatiAlieva@users.noreply.github.com>
2022-05-01 19:03:18 +0300

    Добавила week13

---
+++ week13/assets/js/calc.js
@@ -0,0 +1,26 @@
+function plus() {
+    let a = prompt("Первое число?", 1);
+    let b = prompt("Второе число?", 2);
+
+    alert(Number(a) + Number(b)); // возвращает сумму
+}
+function minus() {
+    let a = prompt("Первое число?", 1);
+    let b = prompt("Второе число?", 2);
+
+    alert(Number(a) - Number(b)); // возвращает разность
+}
+function multiply() {
+    let a = prompt("Первое число?", 1);
+    let b = prompt("Второе число?", 2);
+
+    alert(Number(a) * Number(b)); // возвращает произведение
+}
+function divide() {
+    let a = prompt("Первое число?", 1);
+    let b = prompt("Второе число?", 2);
+
+    alert(Number(a) / Number(b)); // возвращает частное
+}
+
+
